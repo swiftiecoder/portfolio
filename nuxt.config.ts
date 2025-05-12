3d678e240deeb000f5954b3340d75698b195fcef
sherry <95038227+swiftiecoder@users.noreply.github.com>
2025-05-12 20:06:24 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Shaharyar Ahsan Portfolio',
+      title: 'Shaharyar Ahsan',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
