a00d1d6d8b51c1cacc99cdfa226843ddb0622fbd
sherry <95038227+swiftiecoder@users.noreply.github.com>
2025-05-12 20:05:27 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Shaharyar Ahsan Portfolio',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        // You can add other meta tags here
+      ],
+      link: [
+        // You can add global link tags here, like for favicons
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
